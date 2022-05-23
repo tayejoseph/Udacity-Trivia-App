bd1f4ad64175e91c0abbdeadc8e7d6594f2ea9b0
MAC <mac@Taye-Mac.local>
2022-05-23 02:57:44 +0100

    having issues with post question

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,4 +1,6 @@
 import os
+from select import select
+from unicodedata import category
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -8,10 +10,24 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_question(request, selection):
+    page = request.args.get("page", 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
+    CORS(app)
+
+
 
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
@@ -20,12 +36,38 @@ def create_app(test_config=None):
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
+    #CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+        return response
+
 
     """
     @TODO:
     Create an endpoint to handle GET requests
     for all available categories.
     """
+    @app.route("/categories")
+    def get_categories():
+        selection = Category.query.order_by(Category.id).all()
+        
+        if len(selection) == 0:
+            abort(404)
+
+        return jsonify(
+            {
+                "success": True,
+                'categories': [item.format() for item in selection]
+            }
+        )
+
+
 
 
     """
@@ -40,6 +82,26 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
+    @app.route("/questions")
+    def get_questions():
+        selection = Question.query.order_by(Question.id).all()
+        categories = Category.query.order_by(Category.id).all()
+        current_question = paginate_question(request, selection)
+
+        if len(current_question) == 0:
+            abort(404)
+
+        return jsonify(
+            {
+                "success": True,
+                "questions": current_question,
+                "categories": [item.format() for item in categories],
+                "total_questions": len(Question.query.all()),
+                # "current_category": 5
+            }
+        )
+
+
 
     """
     @TODO:
@@ -48,6 +110,31 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
+    @app.route("/questions/<int:question_id>", methods=["DELETE"])
+    def delete_question(question_id): 
+        try:
+            question = Question.query.filter(Question.id == question_id).one_or_none()
+
+            if question is None:
+                abort(404)
+            
+            question.delete()
+            selection = Question.query.order_by(Question.id).all()
+            current_question = paginate_question(request, selection)
+
+            return jsonify(
+                {
+                    "success": True,
+                    "deleted": question_id,
+                    "questions": current_question,
+                    "total_questions": len(Question.query.all())
+                }
+            )
+
+        except:
+            abort(422)
+
+
 
     """
     @TODO:
@@ -59,6 +146,30 @@ def create_app(test_config=None):
     the form will clear and the question will appear at the end of the last page
     of the questions list in the "List" tab.
     """
+    @app.route("/questions", methods=["POST"])
+    def add_question():
+        body = request.get_json()
+
+        question_title = body.get("question", None)
+        question_answer = body.get("answer", None)
+        question_category = body.get("category", None)
+        question_difficulty = body.get("difficulty", None)
+
+        question = Question(question=question_title, answer = question_answer, category = question_category, difficulty = question_difficulty)
+        question.insert()
+
+        selection = Question.query.order_by(Question.id).all()
+        current_questions = paginate_question(request, selection)
+
+        return jsonify(
+            {
+                "success": True,
+                "created": question.id,
+                "questions": current_questions,
+                "total_questions": len(Question.query.all())
+            }
+        )
+
 
     """
     @TODO:
